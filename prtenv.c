ab20a4141be373b4c4094a8e50ea2c6411e43c6c
ngthecoder <ngoto0208@gmail.com>
2025-11-02 14:29:29 -0500

    updated

--- prtenv.c
+++ prtenv.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 void main(){
-    char* shell = getenv("MYSHELL");
+    char* shell = getenv("MYBASH");
     if (shell)
-        printf("%x\n", (unsigned int)shell);
+        printf("MYBASH: %x\n", (unsigned int)shell);
 }
